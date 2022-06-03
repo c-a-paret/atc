3f263318dbc39657f796bff54462de2a2948a5b9
Claude Paret <cparet92@googlemail.com>
2022-06-03 12:12:53 +0100

    Add speed command parser

---
+++ public/CommandParser/CommandParser.js
@@ -0,0 +1,8 @@
+export const parseSpeed = (command) => {
+    const match = command.match(/S(\d{2,3})/g);
+    if (match && match.length === 1) {
+        return parseInt(match[0].substring(1))
+    }
+    return null
+}
+
---
+++ public/CommandParser/testCommandParser.spec.js
@@ -0,0 +1,29 @@
+import {parseSpeed} from "./CommandParser";
+
+describe("Speed Commands", () => {
+    test("Extracts three digit speed from correctly formatted command", () => {
+        const command = "S200"
+        const expectedSpeed = 200
+
+        const result = parseSpeed(command)
+
+        expect(result).toBe(expectedSpeed)
+    })
+
+    test("Extracts two digit speed from correctly formatted command", () => {
+        const command = "S80"
+        const expectedSpeed = 80
+
+        const result = parseSpeed(command)
+
+        expect(result).toBe(expectedSpeed)
+    })
+
+    test("Returns null if no speed command found", () => {
+        const command = "J123H678"
+
+        const result = parseSpeed(command)
+
+        expect(result).toBeNull()
+    })
+})
