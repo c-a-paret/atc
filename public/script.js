29703c8ac0da1cec68783b05482d6dd2a5cd4a8f
Claude Paret <cparet92@googlemail.com>
2022-06-03 18:08:45 +0100

    Just put everything in one fucking file for now

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
-import {parseCommand} from "./CommandParser/CommandParser";
-// const {parseCommand} = require("./CommandParser/CommandParser");
+const parseCommand = (command) => {
+    return {
+        "callSign": command.substring(0, 5),
+        "speed": parseSpeed(command)
+    }
+}
 
+const parseSpeed = (command) => {
+    const match = command.match(/S(\d{2,3})/g);
+    if (match && match.length === 1) {
+        return parseInt(match[0].substring(1))
+    }
+    return null
+}
 
 const initBackgroundLayer = () => {
     const background = document.getElementById("background");
